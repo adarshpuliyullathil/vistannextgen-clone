7ae561f55ccbf1a79c7d539d338cf778c7bf66a9
adarshpuliyullathil <adarshpuliyullathil@gmail.com>
2022-04-19 14:53:25 +0530

    Updates

---
+++ precache-manifest.41ac78eac29f46b9a633b67de15f6f23.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ccdb4a392a055d4f056",
+    "url": "/vistannextgen-clone/static/css/main.7674f01f.chunk.css"
+  },
+  {
+    "revision": "6ccdb4a392a055d4f056",
+    "url": "/vistannextgen-clone/static/js/main.6ccdb4a3.chunk.js"
+  },
+  {
+    "revision": "9614ae3d3918e2042bf6",
+    "url": "/vistannextgen-clone/static/js/1.9614ae3d.chunk.js"
+  },
+  {
+    "revision": "ef4a266f08a4fe20ac16",
+    "url": "/vistannextgen-clone/static/js/2.ef4a266f.chunk.js"
+  },
+  {
+    "revision": "0b89943e2a9a4a19bf79",
+    "url": "/vistannextgen-clone/static/js/runtime~main.0b89943e.js"
+  },
+  {
+    "revision": "ccbabf37aebd6961b04dbb593cce8e7c",
+    "url": "/vistannextgen-clone/static/media/breadcrumb.ccbabf37.jpg"
+  },
+  {
+    "revision": "a5b1de29056bdf9d158399477c76dd9e",
+    "url": "/vistannextgen-clone/static/media/digitalindia.a5b1de29.png"
+  },
+  {
+    "revision": "8c51555b6b0fa46011c666611f20d701",
+    "url": "/vistannextgen-clone/static/media/makeinindia.8c51555b.png"
+  },
+  {
+    "revision": "dfeeeab29a4ded88ac7815c3d48588ef",
+    "url": "/vistannextgen-clone/static/media/01.dfeeeab2.png"
+  },
+  {
+    "revision": "5573fc935af0550e520438ddf8890600",
+    "url": "/vistannextgen-clone/static/media/02.5573fc93.png"
+  },
+  {
+    "revision": "0639513bcb29b712b97d398fc3a8c450",
+    "url": "/vistannextgen-clone/static/media/03.0639513b.png"
+  },
+  {
+    "revision": "b93399325f981f35a23ee4ce4dc6301f",
+    "url": "/vistannextgen-clone/static/media/04.b9339932.png"
+  },
+  {
+    "revision": "3f61ce0e829c17e2a161bbcc2444f973",
+    "url": "/vistannextgen-clone/static/media/06.3f61ce0e.png"
+  },
+  {
+    "revision": "6ec57ccce99b0d8bbdfc0f8f50037e7d",
+    "url": "/vistannextgen-clone/static/media/07.6ec57ccc.png"
+  },
+  {
+    "revision": "1a76e59e8f7e58440c6f51a06dca59e1",
+    "url": "/vistannextgen-clone/static/media/flag.1a76e59e.png"
+  },
+  {
+    "revision": "8ebbf0a1cd7261c9bcef93dace51f997",
+    "url": "/vistannextgen-clone/static/media/robot.8ebbf0a1.png"
+  },
+  {
+    "revision": "3c3520fbee8b99521153c5425b7a301d",
+    "url": "/vistannextgen-clone/static/media/iot.3c3520fb.png"
+  },
+  {
+    "revision": "610a1310245ae6750506c97d3d57e51f",
+    "url": "/vistannextgen-clone/static/media/blockchain.610a1310.png"
+  },
+  {
+    "revision": "a5ddce78e56d405b252ea2db9229f791",
+    "url": "/vistannextgen-clone/static/media/vr.a5ddce78.png"
+  },
+  {
+    "revision": "fa9c067bd5a0ce1e9eff7154e0ce82a7",
+    "url": "/vistannextgen-clone/static/media/digital.fa9c067b.png"
+  },
+  {
+    "revision": "6176494f3244b893c406f27034d957f7",
+    "url": "/vistannextgen-clone/static/media/brain.6176494f.png"
+  },
+  {
+    "revision": "6d330318ca1dca73f0a16b9eec574bdc",
+    "url": "/vistannextgen-clone/static/media/piggy-bank.6d330318.png"
+  },
+  {
+    "revision": "f9e30cc853160369897cb297e21230cb",
+    "url": "/vistannextgen-clone/static/media/doctor.f9e30cc8.png"
+  },
+  {
+    "revision": "c716ed20878cc4299b8abc8f50acfed9",
+    "url": "/vistannextgen-clone/static/media/flunkey.c716ed20.png"
+  },
+  {
+    "revision": "da7045f4cda44a15c0d713460c254ba0",
+    "url": "/vistannextgen-clone/static/media/triaksh.da7045f4.png"
+  },
+  {
+    "revision": "1628b1e87978df87455afcfbc2a06086",
+    "url": "/vistannextgen-clone/static/media/vihr.1628b1e8.png"
+  },
+  {
+    "revision": "9629a2df588257a8b766f249ed689be8",
+    "url": "/vistannextgen-clone/static/media/logo1.9629a2df.png"
+  },
+  {
+    "revision": "2bf0e402b73fb94aa4e77077f9edc396",
+    "url": "/vistannextgen-clone/static/media/robo.2bf0e402.png"
+  },
+  {
+    "revision": "3df5da211ec53a8fe2dc38aa163fd08d",
+    "url": "/vistannextgen-clone/static/media/logo1.3df5da21.png"
+  },
+  {
+    "revision": "7a9213313d9b78d1bf52e35f14227036",
+    "url": "/vistannextgen-clone/static/media/logo2.7a921331.png"
+  },
+  {
+    "revision": "640d1d1592960f076672f8b16ca0b595",
+    "url": "/vistannextgen-clone/static/media/logo3.640d1d15.png"
+  },
+  {
+    "revision": "93bd2e3ba39d02f1a1066bcdb9e18185",
+    "url": "/vistannextgen-clone/static/media/bg.93bd2e3b.png"
+  },
+  {
+    "revision": "e1be867a1f6359175f641750cac88ac8",
+    "url": "/vistannextgen-clone/static/media/bg.e1be867a.jpg"
+  },
+  {
+    "revision": "a752b5cd62861b305abda92494e2062e",
+    "url": "/vistannextgen-clone/index.html"
+  }
+];
